ad552182731c4e2b4e4e21756a201e9ce9781e6d
dimastatz <dima.statz@gmail.com>
2021-08-28 22:47:20 +0300

    add holding period return

--- src/single_stock.py
+++ src/single_stock.py
@@ -1,19 +1,26 @@
-import numpy as np
 import pandas as pd
 from pandas_datareader import data
-from pandas.core.frame import DataFrame
 from pandas.core.series import Series
+from pandas.core.frame import DataFrame
+
+
+# holding period return in percents
+def get_holding_period_return(df: DataFrame, start, end, col) -> float:
+    start_price = df.at[pd.to_datetime(start), col]
+    end_price = df.at[pd.to_datetime(end), col]
+    return (end_price - start_price) * 100 / start_price
 
 
 # calculates return for the last x days in percents
 def get_rolling_x_day_return(df: DataFrame, col: str, days: int) -> DataFrame:
-    f = lambda x: round((x[-1:] - x[0]) * 100 / x[0], 2) 
+    def f(x: Series):
+        return round((x[-1:] - x[0]) * 100 / x[0], 2)
+
     df['Rolling Return'] = df[col].rolling(days).apply(f)
     return df
 
 
-# read stock data from yahoo finance 
-# and return adjusted cose prices
+# read stock data from yahoo finance and return adjusted cose prices
 def read_stock_dataframe(start: str, end: str, symbol: str) -> DataFrame:
     df = data.get_data_yahoo(symbol, start, end)
     return df[['Adj Close']]
--- tests/test_single_stock.py
+++ tests/test_single_stock.py
@@ -1,4 +1,5 @@
 import unittest
+import pandas as pd
 from src.single_stock import *
 
 
@@ -7,8 +8,12 @@ class TestSingleStock(unittest.TestCase):
         symbol = ('MSFT', '2012-01-01', '2012-01-31')
         df = read_stock_dataframe(symbol[1], symbol[2], symbol[0])
         self.assertEqual(20, df.shape[0])
-        print(df)
+        print('HEY', pd.to_datetime('2012-01-03'))
+        print(df.at[pd.to_datetime('2012-01-03'), 'Adj Close'])
 
         df = get_rolling_x_day_return(df, 'Adj Close', 10)
+        print(df)
+
+        df = get_holding_period_return(df, '2012-01-03', '2012-01-09', 'Adj Close')
         print(df)
         
