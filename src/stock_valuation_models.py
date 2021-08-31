a6c37f5e50c603f08e91206c98708392b2815318
dimastatz <dima.statz@gmail.com>
2021-08-31 23:46:56 +0300

    add stock valuation

---
+++ src/stock_valuation_models.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from pandas.core.frame import DataFrame
+
+
+class DividentDiscountModel:
+    def __init__(self, df: DataFrame , payout_ratio: float = 50) -> None:
+        self.payout_ratio = payout_ratio
+
+    def calulate(df: DataFrame):
+        pass
