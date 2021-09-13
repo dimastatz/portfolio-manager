dbe80d3628baffbebbfc2b222437a8d000e35f43
dimastatz <dima.statz@gmail.com>
2021-09-13 11:56:57 +0300

    test markdown in j notebooks

--- tests/test.py
+++ tests/test.py
@@ -1,19 +1,6 @@
-import time
-import requests
-import concurrent.futures
 
-h = { 'apikey': '7575378a-446c-48df-aadc-c95c31b7ad92' }
-url = 'https://data-collector-stage.video-data-platform-prod.aws.oath.cloud/batch'
+data = [i for i in range(100)]
+chunks = [data[x:x+10] for x in range(0, len(data), 10)]
+res = ['\n'.join([str(i) for i in x]) for x in chunks]
 
-
-start = time.time()
-lst = []
-with concurrent.futures.ThreadPoolExecutor(max_workers=8) as e:
-    for i in range(0, 10):
-        lst.append(e.submit(requests.post, url, headers = h, data = '1'))
-
-for x in concurrent.futures.as_completed(lst):
-    print(x.result().status_code)
-    raise Exception('failed ' + )
-
-print(time.time() - start)
+print(res[0])
