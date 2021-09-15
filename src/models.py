79498ca053e3aac29034a9ab85b56b635a1185d3
dimastatz <dima.statz@gmail.com>
2021-09-15 09:53:08 +0300

    add ideas document

--- src/models.py
+++ src/models.py
@@ -1,13 +1,14 @@
 from typing import NamedTuple
 
-class DividentDiscountParams(NamedTuple):
+
+class DividendDiscountParams(NamedTuple):
     net_income: float 
     growth_rate: float
     discount_rate: float
     shares_outstanding: float
-    payout_ratio: float = 50
+    payout_ratio: float = 0.5
 
 
-def get_divident_discount(p: DividentDiscountParams) -> float:
+def get_divident_discount(p: DividendDiscountParams) -> float:
     divident_next_year = (p.net_income / p.shares_outstanding) * p.payout_ratio
     return divident_next_year / (p.discount_rate - p.growth_rate)
