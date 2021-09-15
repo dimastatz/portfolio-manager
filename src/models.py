526a67508e91c34918239cc3148c3287f3ae4259
dimastatz <dima.statz@gmail.com>
2021-09-15 10:27:19 +0300

    add random model

--- src/models.py
+++ src/models.py
@@ -1,5 +1,25 @@
-from typing import NamedTuple
+import random
+from typing import List, NamedTuple
 
+class Model:
+    def __init__(self, name: str):
+        self.name = name
+    
+    def run(self) -> str:
+        raise Exception('Run is not implemented in Abstract Model')
+
+
+class RandomModel(Model):
+    def __init__(self, name: str):
+        super().__init__(name)
+
+    def run(self) -> str:
+        return 'Buy' if bool(random.getrandbits(1)) else 'Sell'
+
+
+def get_models() -> List[Model]:
+    return [RandomModel('Model_1'), RandomModel('Model_2'), RandomModel('Model_3')]
+       
 
 class DividendDiscountParams(NamedTuple):
     net_income: float 
@@ -8,6 +28,13 @@ class DividendDiscountParams(NamedTuple):
     shares_outstanding: float
     payout_ratio: float = 0.5
 
+
+class DividendDiscountParams(Model):
+    def __init__(self, name: str, params: DividendDiscountParams):
+        super().__init__(name) 
+        self.params = params
+
+    def
 
 def get_divident_discount(p: DividendDiscountParams) -> float:
     divident_next_year = (p.net_income / p.shares_outstanding) * p.payout_ratio
